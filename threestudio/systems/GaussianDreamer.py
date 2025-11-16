0a5bdb007a131969c4ceb0a7d4313616bb08fb5a
crab713 <835314623@qq.com>
2025-11-17 00:02:27 +0800

    增加vsd相关处理

--- threestudio/systems/GaussianDreamer.py
+++ threestudio/systems/GaussianDreamer.py
@@ -13,9 +13,8 @@ from gaussiansplatting.scene.gaussian_model import BasicPointCloud
 
 import threestudio
 from threestudio.systems.base import BaseLift3DSystem
-from threestudio.utils.ops import binary_cross_entropy,dot
+from threestudio.utils.ops import binary_cross_entropy
 from threestudio.utils.typing import *
-from threestudio.utils.loss import tv_loss
 
 
 @threestudio.register("gaussiandreamer-system")
@@ -24,12 +23,17 @@ class GaussianDreamer(BaseLift3DSystem):
     class Config(BaseLift3DSystem.Config):
         radius: float = 4
         sh_degree: int = 0
+        dreambooth: int = 0
         load_type: int = 0
         load_path: str = "./load/shapes/stand.obj"
+        loss_type: str = "vsd"
+
 
     # ================== 初始化相关 ==========================
     cfg: Config
     def configure(self) -> None:
+        self.loss_type = self.cfg.loss_type
+        self.dreambooth = self.cfg.dreambooth
         self.radius = self.cfg.radius
         self.sh_degree =self.cfg.sh_degree
         self.load_type =self.cfg.load_type
@@ -41,8 +45,6 @@ class GaussianDreamer(BaseLift3DSystem):
 
     def pcd_init(self) -> BasicPointCloud:
         """加载点云数据, 并处理成3dgs的点云格式"""
-        coords,rgb,conf = None,None,None
-
         # Since this data set has no colmap data, we start with random points
         if self.load_type== 4: # shap_e
             from threestudio.systems.function.point_cloud import load_from_shape
@@ -59,67 +61,68 @@ class GaussianDreamer(BaseLift3DSystem):
         elif self.load_type == 0: # vggt
             from threestudio.systems.function.point_cloud import load_from_vggt
             save_path = self.get_save_path('instance_images/')
-            coords,rgb,conf = load_from_vggt(self.cfg, save_path)
+            coords,rgb = load_from_vggt(self.cfg, save_path)
         else:
             raise NotImplementedError(f"load_type {self.load_type} is not implemented, only support [0: shap_e, 1: pcd, 2: smpl, 3: 3dgs]")
-        if conf is None:
-            conf = np.ones((coords.shape[0]))
-        # bound = self.radius * 0.75
-        x_length = np.max(coords[:, 0]) - np.min(coords[:, 0])
-        y_length = np.max(coords[:, 1]) - np.min(coords[:, 1])
-        z_length = np.max(coords[:, 2]) - np.min(coords[:, 2])
-        max_length = max(x_length, y_length, z_length)
-        ratio = self.radius / max_length
-
-        pcd = BasicPointCloud(points=coords*ratio, colors=rgb, normals=np.zeros((coords.shape[0], 3)), confidences=conf)
-
+        
+        bound = self.radius * 2
+        pcd = BasicPointCloud(points=coords*bound, colors=rgb, normals=np.zeros((coords.shape[0], 3)))
         return pcd
     
     def on_fit_start(self) -> None:
         super().on_fit_start()
 
-        # # 如果本地存在instance image, 则可以finetune
-        # if os.path.exists(os.path.join(self.get_save_dir(), 'instance_images/')):
-        #     # finetune guidance(需要本地下载好模型)
-        #     cmd = [
-        #         "python", "threestudio/systems/function/dreambooth.py",
-        #         "--pretrained_model_name_or_path", self.cfg.guidance.pretrained_model_name_or_path,
-        #         "--enable_xformers_memory_efficient_attention",
-        #         "--with_prior_preservation",
-        #         "--instance_data_dir", self.get_save_path('instance_images/'),
-        #         "--instance_prompt", self.cfg.prompt_processor.prompt,
-        #         "--class_data_dir", self.get_save_path('class_samples/'),
-        #         "--class_prompt", self.cfg.prompt_processor.prompt.replace('<kth>', ''),
-        #         "--validation_prompt", self.cfg.prompt_processor.prompt,
-        #         "--output_dir", self.get_save_path('personalization/'),
-        #         "--max_train_steps", str(4000),
-        #         "--train_batch_size", str(1),
-        #         "--gradient_accumulation_steps", str(4),
-        #         "--mixed_precision", "fp16"
-        #     ]
-        #     # 执行dreambooth训练
-        #     import subprocess
-        #     try:
-        #         result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        #         print("DreamBooth训练成功完成")
-        #         print(result.stdout)
-        #         if result.returncode != 0:
-        #             raise subprocess.CalledProcessError(result.returncode, cmd, result.stdout, result.stderr)
-        #     except subprocess.CalledProcessError as e:
-        #         print("DreamBooth训练失败")
-        #         print(f"错误输出: {e.stderr}")
-        #         print(f"返回码: {e.returncode}")
+        # 根据mvadapter来进行微调
+        if self.cfg.dreambooth == 1:
+            cmd = [
+                "python", "threestudio/systems/function/dreambooth.py",
+                "--pretrained_model_name_or_path", self.cfg.guidance.pretrained_model_name_or_path,
+                "--enable_xformers_memory_efficient_attention",
+                "--with_prior_preservation",
+                "--instance_data_dir", self.get_save_path('instance_images/'),
+                "--instance_prompt", self.cfg.prompt_processor.dreambooth_prompt,
+                "--class_data_dir", self.get_save_path('class_samples/'),
+                "--class_prompt", self.cfg.prompt_processor.prompt.replace('<kth>', ''),
+                "--num_class_images", "100",
+                "--validation_prompt", self.cfg.prompt_processor.prompt,
+                "--output_dir", self.get_save_path('personalization/'),
+                "--max_train_steps", str(250),
+                "--train_batch_size", str(8),
+                "--gradient_accumulation_steps", str(1),
+                # "--mixed_precision", "fp16"
+            ]
+            # 执行dreambooth训练
+            import subprocess
+            try:
+                result = subprocess.run(cmd, check=True)
+                print("DreamBooth训练成功完成")
+                print(result.stdout)
+                if result.returncode != 0:
+                    raise subprocess.CalledProcessError(result.returncode, cmd, result.stdout, result.stderr)
+            except subprocess.CalledProcessError as e:
+                print("DreamBooth训练失败")
+                print(f"错误输出: {e.stderr}")
+                print(f"返回码: {e.returncode}")
         
-        # # only used in training
-        # self.prompt_processor = threestudio.find(self.cfg.prompt_processor_type)(
-        #     self.get_save_path('personalization/')
-        # )
-        # self.guidance = threestudio.find(self.cfg.guidance_type)(self.get_save_path('personalization/'))
+            # only used in training
+            self.cfg.prompt_processor.pretrained_model_name_or_path = self.get_save_path('personalization/')
+            self.cfg.guidance.pretrained_model_name_or_path = self.get_save_path('personalization/')
+            if self.loss_type == "vsd":
+                self.cfg.guidance.pretrained_model_name_or_path_lora = self.get_save_path('personalization/')
         self.prompt_processor = threestudio.find(self.cfg.prompt_processor_type)(
             self.cfg.prompt_processor
         )
         self.guidance = threestudio.find(self.cfg.guidance_type)(self.cfg.guidance)
-        self.prompt_utils = self.prompt_processor()
+        
+        # 优化器
+        # if self.loss_type == "vsd":
+        #     optimizer = self.gaussian.optimizer
+        #     new_param_group = {
+        #         'params': [p for p in self.guidance.submodules.pipe_lora.unet.parameters() if p.requires_grad],
+        #         'lr': 1e-4,          
+        #         'weight_decay': 1e-2
+        #     }
+        #     optimizer.add_param_group(new_param_group)
 
     def configure_optimizers(self):
         self.parser = ArgumentParser(description="Training script parameters")
@@ -136,11 +139,10 @@ class GaussianDreamer(BaseLift3DSystem):
         from threestudio.systems.function.point_cloud import save_ply, load_from_3dgs
         coords, rgb = load_from_3dgs(save_path)
         save_ply(self.get_save_path(f"init-color.ply"), coords, rgb)
-
         # 准备训练
         self.pipe = PipelineParams(self.parser)
         self.gaussian.training_setup(opt)
-        
+
         ret = {
             "optimizer": self.gaussian.optimizer,
         }
@@ -189,45 +191,32 @@ class GaussianDreamer(BaseLift3DSystem):
 
         out = self(batch) 
 
-        
+        prompt_utils = self.prompt_processor()
         images = out["comp_rgb"]
 
         guidance_eval = (self.true_global_step % 200 == 0)        
         guidance_out = self.guidance(
-            images, self.prompt_utils, **batch, rgb_as_latents=False,guidance_eval=guidance_eval
+            images, prompt_utils, **batch, rgb_as_latents=False,guidance_eval=guidance_eval
         )
         
-        
-        # loss = 0.0
-
-        
-        # loss = loss + guidance_out['loss_sds'] *self.C(self.cfg.loss['lambda_sds'])
-        # loss_sparsity = (out["opacity"] ** 2 + 0.01).sqrt().mean()
-        # self.log("train/loss_sparsity", loss_sparsity)
-        # loss += loss_sparsity * self.C(self.cfg.loss.lambda_sparsity)
-
-        # opacity_clamped = out["opacity"].clamp(1.0e-3, 1.0 - 1.0e-3)
-        # loss_opaque = binary_cross_entropy(opacity_clamped, opacity_clamped)
-        # self.log("train/loss_opaque", loss_opaque)
-        # loss += loss_opaque * self.C(self.cfg.loss.lambda_opaque)
-        loss = self.compute_vsd_loss(guidance_out,out)
-        
-        
-        
-        
-        # if guidance_eval:
-        #     self.guidance_evaluation_save(
-        #         out["comp_rgb"].detach()[: guidance_out["eval"]["bs"]],
-        #         guidance_out["eval"],
-        #     )
+        if self.loss_type == "sds":
+            loss = self.compute_sds_loss(guidance_out,out)
+            if guidance_eval:
+                self.guidance_evaluation_save(
+                    out["comp_rgb"].detach()[: guidance_out["eval"]["bs"]],
+                    guidance_out["eval"],
+                )
+        elif self.loss_type == "vsd":
+            loss = self.compute_vsd_loss(guidance_out,out)
         for name, value in self.cfg.loss.items():
             self.log(f"train_params/{name}", self.C(value))
-        self.log(f"train/loss", loss)
         return {"loss": loss}
 
     def on_before_optimizer_step(self, optimizer):
         with torch.no_grad():
-            if self.true_global_step < 900: # 1200
+            if self.true_global_step < 3000: # 原90
+                if self.true_global_step ==500 or self.true_global_step ==1000:
+                    self.gaussian.oneupSHdegree()
                 viewspace_point_tensor_grad = torch.zeros_like(self.viewspace_point_list[0])
                 for idx in range(len(self.viewspace_point_list)):
                     viewspace_point_tensor_grad = viewspace_point_tensor_grad + self.viewspace_point_list[idx].grad
@@ -236,9 +225,15 @@ class GaussianDreamer(BaseLift3DSystem):
                 
                 self.gaussian.add_densification_stats(viewspace_point_tensor_grad, self.visibility_filter)
 
-                if self.true_global_step > 300 and self.true_global_step % 100 == 0: # 500 100
+                if 300 < self.true_global_step < 900 and self.true_global_step % 100 == 0: # 500 100
                     size_threshold = 20 if self.true_global_step > 500 else None # 3000
-                    self.gaussian.densify_and_prune(0.0002 , 0.05, self.cameras_extent, size_threshold)
+                    self.gaussian.densify_and_prune(0.005, 0.05, self.cameras_extent, size_threshold) 
+                else:
+                    size_threshold = 20
+                    if self.true_global_step % 200 == 0:
+                        self.gaussian.densify(0.005, self.cameras_extent)
+                    if self.true_global_step % 100 == 0:
+                        self.gaussian.prune(0.05, self.cameras_extent, size_threshold)
 
 
     # ================== 验证测试相关 ==========================
@@ -278,13 +273,9 @@ class GaussianDreamer(BaseLift3DSystem):
             name="validation_step",
             step=self.true_global_step,
         )
-        
         # save_path = self.get_save_path(f"it{self.true_global_step}-val.ply")
         # self.gaussian.save_ply(save_path)
-        # # 保存转换到rgb空间的点云
-        # from threestudio.systems.function.point_cloud import save_ply, load_from_3dgs
-        # coords, rgb = load_from_3dgs(save_path)
-        # save_ply(self.get_save_path(f"it{self.true_global_step}-val-color.ply"), coords, rgb)
+        # load_ply(save_path,self.get_save_path(f"it{self.true_global_step}-val-color.ply"))
 
     def on_validation_epoch_end(self):
         pass
